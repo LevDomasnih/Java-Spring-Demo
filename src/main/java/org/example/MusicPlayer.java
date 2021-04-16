2ad37eefab65d9006c01c2438346929202b043f4
Lev Domasnih <lev.domasnih@motus-group.ru>
2021-04-16 23:24:30 +0500

    ф

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class MusicPlayer {
+    private Music music;
+
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
