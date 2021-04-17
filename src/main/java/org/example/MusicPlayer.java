e27c5a0f0ba1936caf692a17be5c0dc764c5b911
LevDomasnih <lev.domasnix2013@gmail.com>
2021-04-17 19:44:44 +0500

    Dependency Injection

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,18 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    public MusicPlayer() {}
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+    public void playMusicList() {
+        for(Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
